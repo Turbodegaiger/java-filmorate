38788c0c9ebba1401335ff7fb018728d2676d46b
DmitryPrivalov <apothecary1@yandex.ru>
2023-05-15 18:38:35 +0300

    feat: переделаны data.sql и schema.sql под синтаксис H2 v4

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,16 @@
-INSERT INTO genres (genre_id, name)
-VALUES (1, 'Комедия'), 
-(2, 'Драма'), 
-(3, 'Мультфильм'), 
-(4, 'Триллер'), 
-(5, 'Документальный'), 
-(6, 'Боевик')
-ON CONFLICT DO NOTHING;
+MERGE INTO genres AS g USING (VALUES (1, 'Комедия'),
+(2, 'Драма'),
+(3, 'Мультфильм'),
+(4, 'Триллер'),
+(5, 'Документальный'),
+(6, 'Боевик')) s(genre_id, name)
+ON g.genre_id = s.genre_id AND g.name = s.name
+WHEN NOT MATCHED THEN INSERT VALUES (s.genre_id, s.name);
 
-INSERT INTO mpa (mpa_id, name)
-VALUES (1, 'G'), 
-(2, 'PG'), 
-(3, 'PG-13'), 
-(4, 'R'), 
-(5, 'NC-17')
-ON CONFLICT DO NOTHING;
+MERGE INTO mpa AS m USING (VALUES (1, 'G'),
+(2, 'PG'),
+(3, 'PG-13'),
+(4, 'R'),
+(5, 'NC-17')) s(mpa_id, name)
+ON m.mpa_id = s.mpa_id AND m.name = s.name
+WHEN NOT MATCHED THEN INSERT VALUES (s.mpa_id, s.name);
